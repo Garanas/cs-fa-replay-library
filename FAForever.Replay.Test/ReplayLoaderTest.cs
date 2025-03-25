b372a34df806afc8530c1578ac5f9c86c76ef306
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2025-03-25 09:03:24 +0100

    Add additional tests

--- FAForever.Replay.Test/ReplayLoaderTest.cs
+++ FAForever.Replay.Test/ReplayLoaderTest.cs
@@ -4,6 +4,7 @@ namespace FAForever.Replay.Test;
 public class ReplayLoaderTest
 {
     [TestMethod]
+    [TestCategory("FAFReplay")]
     [DataRow("assets/faforever/zstd/22338092.fafreplay")]
     [DataRow("assets/faforever/zstd/22373098.fafreplay")]
     [DataRow("assets/faforever/zstd/22425616.fafreplay")]
@@ -14,6 +15,7 @@ public class ReplayLoaderTest
     }
 
     [TestMethod]
+    [TestCategory("FAFReplay")]
     [DataRow("assets/faforever/gzip/22451957.fafreplay")]
     [DataRow("assets/faforever/gzip/22453414.fafreplay")]
     [DataRow("assets/faforever/gzip/22453511.fafreplay")]
@@ -24,6 +26,7 @@ public class ReplayLoaderTest
     }
 
     [TestMethod]
+    [TestCategory("FAFReplay")]
     [DataRow("assets/faforever/zstd/22338092.fafreplay", 12)]
     [DataRow("assets/faforever/zstd/22373098.fafreplay", 123)]
     [DataRow("assets/faforever/zstd/22425616.fafreplay", 99)]
@@ -36,6 +39,8 @@ public class ReplayLoaderTest
     [DataRow("assets/faforever/23225440.fafreplay", 6630)]
     [DataRow("assets/faforever/23225685.fafreplay", 14178)]
     [DataRow("assets/faforever/23225104.fafreplay", 51017)]
+    [DataRow("assets/faforever/23555859.fafreplay", 12795)]
+    [DataRow("assets/faforever/23962051.fafreplay", 15480)]
     [DataRow("assets/faforever/ai/23374795-zhanghm18.fafreplay", 567)]
     public void FAForeverUserInputCountTest(string file, int expectedCount)
     {
@@ -44,9 +49,11 @@ public class ReplayLoaderTest
     }
 
     [TestMethod]
+    [TestCategory("SCFAReplay")]
     [DataRow("assets/scfa/balthazar-01.SCFAReplay", 6290)]
     [DataRow("assets/scfa/balthazar-02.SCFAReplay", 8377)]
     [DataRow("assets/scfa/balthazar-03.SCFAReplay", 12077)]
+    [DataRow("assets/scfa/23555859.SCFAReplay", 12795)]
     public void SCFAUserInputCountTest(string file, int expectedCount)
     {
         Replay replay = ReplayLoader.LoadSCFAReplayFromDisk(file);
@@ -54,6 +61,7 @@ public class ReplayLoaderTest
     }
 
     [TestMethod]
+    [TestCategory("FAFReplay")]
     [DataRow("assets/faforever/TestCommands01.fafreplay", 106)]
     [DataRow("assets/faforever/gzip/22451957.fafreplay", 0)]
     [DataRow("assets/faforever/gzip/22453511.fafreplay", 3)]
@@ -62,23 +70,5 @@ public class ReplayLoaderTest
         Replay replay = ReplayLoader.LoadFAFReplayFromDisk(file);
         List<ReplayChatMessage> chatMessages = ReplaySemantics.GetChatMessages(replay);
         Assert.AreEqual(expectedCount, chatMessages.Count);
-    }
-
-    [TestMethod]
-    [DataRow("assets/faforever/mods.fafreplay", 2)]
-    public void FAForeverModCountTest(string file, int expectedCount)
-    {
-        Replay replay = ReplayLoader.LoadFAFReplayFromDisk(file);
-
-        // TODO
-    }
-
-    [TestMethod]
-    [DataRow("assets/scfa/21stGameOceanScampsV2.SCFAReplay", 0)]
-    public void SCFAModCountTest(string file, int expectedCount)
-    {
-        Replay replay = ReplayLoader.LoadSCFAReplayFromDisk(file);
-
-        // TODO
     }
 }
